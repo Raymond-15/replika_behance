0c00ef507b3e23fb018396df7b989a9d54f393d9
Raymond-15 <raymondputra15@gmail.com>
2021-07-25 23:17:33 +0800

    first

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+  <link rel="shortcut icon" href="assets/img/behance-icon.png" />
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+
+  <link rel="stylesheet" href="assets/css/style.css" />
+
+  <link href="assets/fontawesome/css/all.css" rel="stylesheet" />
+  <!--load all styles -->
+
+  <title>Behance</title>
+</head>
+
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light pad-nav">
+    <a class="navbar-brand" href="#"><img src="assets/img/Behance_logo.png" class="logo" alt="" /></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Discover <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Livestreams</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Jobs</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item pl-3">
+          <a class="btn btn-outline-secondary lengkung" href="#">Log In</a>
+        </li>
+        <li class="nav-item pl-3">
+          <a class="btn btn-primary lengkung" href="#">Sign Up</a>
+        </li>
+        <li class="pl-3">
+          <img id="adobe" src="assets/img/adobe-systems.png" alt="" />
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <!-- Akhir Nav -->
+
+  <!-- header -->
+  <header class="pad-nav mt-4 pencarian">
+    <div>
+      <form class="input-group">
+        <!-- <span class="fas fa-tools"></span> -->
+        <input id="search" type="text" class="col-7" name="search" placeholder="Search the creative world at work" />
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary rounded btn-sm ml-2" type="button" id="button-addon2">Projects</button>
+          <button class="btn btn-outline-secondary lengkung ml-2" type="button" id="button-addon2">Images</button>
+          <button class="btn btn-outline-secondary lengkung ml-2" type="button" id="button-addon2">Prototypes</button>
+          <button class="btn btn-outline-secondary lengkung ml-2" type="button" id="button-addon2">People</button>
+          <button class="btn btn-outline-secondary lengkung ml-2" type="button" id="button-addon2">Moodboards</button>
+        </div>
+      </form>
+
+    </div>
+  </header>
+  <!-- akhir header -->
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+
+</html>
