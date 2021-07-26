40e6816f4e086dbd70b0c04fb6e0e9cdd2c3e80f
Raymond-15 <raymondputra15@gmail.com>
2021-07-27 06:43:53 +0800

    perbaiki font & ukuran

--- index.php
+++ index.php
@@ -20,62 +20,75 @@
 </head>
 
 <body>
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light pad-nav">
-    <a class="navbar-brand" href="#"><img src="assets/img/Behance_logo.png" class="logo" alt="" /></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Discover <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Livestreams</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Jobs</a>
-        </li>
-      </ul>
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item pl-3">
-          <a class="btn btn-outline-secondary lengkung" href="#">Log In</a>
-        </li>
-        <li class="nav-item pl-3">
-          <a class="btn btn-primary lengkung" href="#">Sign Up</a>
-        </li>
-        <li class="pl-3">
-          <img id="adobe" src="assets/img/adobe-systems.png" alt="" />
-        </li>
-      </ul>
-    </div>
-  </nav>
-  <!-- Akhir Nav -->
+  <section class=" fixed-top">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light pad-nav">
+      <a class="navbar-brand" href="#"><img src="assets/img/Behance_logo.png" class="logo" alt="" /></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-body font-weight-bold" title="Discover" href="#">Discover</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-body font-weight-bold" title="Livestreams" href="#">Livestreams</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-body font-weight-bold" title="Jobs" href="#">Jobs</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item pl-3">
+            <a class="btn btn-outline-secondary text-body btn-sm lengkung" href="#" role="button">Log In</a>
+          </li>
+          <li class="nav-item pl-3">
+            <a class="btn btn-primary btn-sm lengkung" href="#">Sign Up</a>
+          </li>
+          <li class="pl-3 ml-4">
+            <a href="#">
+              <img src="assets/img/adobe-systems.png" id="adobe" alt="">
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- Akhir Nav -->
 
-  <!-- header -->
-  <header class="pad-nav mt-4 pencarian">
-    <div>
+    <!-- header -->
+    <div class="mt-4 pencarian row-lg">
       <form class="input-group">
-        <!-- <span class="fas fa-tools"></span> -->
-        <input id="search" type="text" class="col-7" name="search" placeholder="Search the creative world at work" />
-        <div class="input-group-append">
-          <button class="btn btn-outline-secondary rounded btn-sm ml-2" type="button" id="button-addon2">Projects</button>
-          <button class="btn btn-outline-secondary lengkung ml-2" type="button" id="button-addon2">Images</button>
-          <button class="btn btn-outline-secondary lengkung ml-2" type="button" id="button-addon2">Prototypes</button>
-          <button class="btn btn-outline-secondary lengkung ml-2" type="button" id="button-addon2">People</button>
-          <button class="btn btn-outline-secondary lengkung ml-2" type="button" id="button-addon2">Moodboards</button>
-        </div>
+        <input id="search" type="text" class="col-lg" name="search" placeholder="Search the creative world at work" />
+        <button class="tomSearch ml-3" type="button">Projects</button>
+        <button class="tomSearch ml-2" type="button">Images</button>
+        <button class="tomSearch ml-2" type="button">Prototypes</button>
+        <button class="tomSearch ml-2" type="button">People</button>
+        <button class="tomSearch ml-2 mr-3" type="button">Moodboards</button>
       </form>
 
     </div>
-  </header>
-  <!-- akhir header -->
+    <!-- akhir header -->
+
+    <!-- <div class="row-lg">
+      <button></button>
+    </div> -->
+
+  </section>
+
+
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $(document).tooltip();
+    });
+  </script>
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src=" https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/bcce6f187a.js" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 
